8ebc26b33dd425162ef7415023026b98f951bf6e
ThisNarciss <tyomaxxx000@gmail.com>
2023-01-12 20:03:21 +0200

    add current page style

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -16,47 +16,58 @@ export default function pagination(currentPage, allPages) {
   let afterTwoPage = currentPage + 2;
   let afterOnePage = currentPage + 1;
 
-  if (currentPage >= 1) {
+  if (currentPage > 1) {
+    murkUp +=
+      '<li class="item-pag"><a class="link-pag link-pag--btn " href=""><svg class="pag-icon pag-icon--reverse" width="30" height="30"><use href="/symbol-defs.a8b2e413.svg#icon-btn-arrow"></use></svg></a></li>';
+  } else if (currentPage === 1) {
     murkUp +=
       '<li class="item-pag"><a class="link-pag link-pag--btn link-pag--hidden" href=""><svg class="pag-icon pag-icon--reverse" width="30" height="30"><use href="/symbol-defs.a8b2e413.svg#icon-btn-arrow"></use></svg></a></li>';
   }
 
   if (currentPage > 1) {
-    murkUp += '<li class="item-pag"><a class="link-pag" href="">1</a></li>';
+    murkUp +=
+      '<li class="item-pag"><a id="first" class="link-pag" href="">1</a></li>';
   }
   if (currentPage > 4) {
-    murkUp += '<li class="item-pag"><a class="link-pag" href="">...</a></li>';
+    murkUp +=
+      '<li class="item-pag"><a id="first" class="link-pag" href="">...</a></li>';
   }
   if (currentPage > 3) {
-    murkUp += `<li class="item-pag"><a class="link-pag" href="">${beforeTwoPage}</a></li>`;
+    murkUp += `<li class="item-pag"><a id="first" class="link-pag" href="">${beforeTwoPage}</a></li>`;
   }
   if (currentPage > 2) {
-    murkUp += `<li class="item-pag"><a class="link-pag" href="">${beforeOnePage}</a></li>`;
+    murkUp += `<li class="item-pag"><a id="first" class="link-pag" href="">${beforeOnePage}</a></li>`;
   }
   murkUp += `<li class="item-pag">
-        <a class="link-pag" href="">${currentPage}</a>
+        <a id="first" class="link-pag" href="">${currentPage}</a>
       </li>`;
   if (allPages - 1 > currentPage) {
-    murkUp += `<li class="item-pag"><a class="link-pag" href="">${afterOnePage}</a></li>`;
+    murkUp += `<li class="item-pag"><a id="first" class="link-pag" href="">${afterOnePage}</a></li>`;
   }
   if (allPages - 2 > currentPage) {
-    murkUp += `<li class="item-pag"><a class="link-pag" href="">${afterTwoPage}</a></li>`;
+    murkUp += `<li class="item-pag"><a id="first" class="link-pag" href="">${afterTwoPage}</a></li>`;
   }
   if (allPages - 3 > currentPage) {
-    murkUp += '<li class="item-pag"><a class="link-pag" href="">...</a></li>';
+    murkUp +=
+      '<li class="item-pag"><a id="first" class="link-pag" href="">...</a></li>';
   }
   if (allPages > currentPage) {
     murkUp += `<li class="item-pag">
-        <a class="link-pag" href="">${allPages}</a>
+        <a id="first" class="link-pag" href="">${allPages}</a>
       </li>`;
   }
 
-  if (allPages >= currentPage) {
+  if (allPages > currentPage) {
+    murkUp +=
+      '<li class="item-pag"><a class="link-pag link-pag--btn " href=""><svg class="pag-icon" width="30" height="30"><use href="/symbol-defs.a8b2e413.svg#icon-btn-arrow"></use></svg></a></li>';
+  } else if (allPages === currentPage) {
     murkUp +=
       '<li class="item-pag"><a class="link-pag link-pag--btn link-pag--hidden" href=""><svg class="pag-icon" width="30" height="30"><use href="/symbol-defs.a8b2e413.svg#icon-btn-arrow"></use></svg></a></li>';
   }
 
   listRef.innerHTML = murkUp;
+  const firstRef = document.getElementById('first');
+  firstRef.classList.add('current');
 }
 
 listRef.addEventListener('click', renderPaginationMurkUp);
@@ -69,11 +80,16 @@ function renderPaginationMurkUp(evt) {
   if (evt.target.textContent === '...') {
     return;
   }
+  // if (evt.target)
   const page = Number(evt.target.textContent);
   fetchCountries(page).then(data => {
     const allPages = Math.ceil(data.totalHits / data.hits.length);
     console.log(allPages);
     createMurkUp(data.hits, dataBox);
     pagination(page, allPages);
+    if (evt.target.textContent === page.toString()) {
+      evt.target.classList.add('current');
+      console.log(evt.target);
+    }
   });
 }
